76e25689158cee929ef2d5a3765c5da6de0c85ac
fayis <fayisak456@gmail.com>
2024-03-05 13:31:24 +0530

    Initial commit

---
+++ lib/src/app.dart
@@ -0,0 +1,22 @@
+
+import 'package:colleges_api/src/screens/university_details_screen.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+     
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: UniversityDetailsScreen(),
+    );
+  }
+}
---
+++ lib/src/screens/university_details_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UniversityDetailsScreen extends StatelessWidget {
+  const UniversityDetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('UniversityDetailsscreen'),),
+    );
+  }
+}
